e698df2eb7838caa3c4194f5848ac31d39f9ff0d
xmh1011 <1197843839@qq.com>
2022-10-04 15:30:27 +0800

    10.4 commit

---
+++ go/5.go
@@ -0,0 +1,58 @@
+package main
+
+//给你一个字符串 s，找到 s 中最长的回文子串。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "cbbd"
+//输出："bb"
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 1000
+// s 仅由数字和英文字母组成
+//
+//
+// Related Topics 字符串 动态规划 👍 5774 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func longestPalindrome(s string) string {
+	if s == "" {
+		return ""
+	}
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		left1, right1 := expandAroundCenter(s, i, i)
+		left2, right2 := expandAroundCenter(s, i, i+1)
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	for ; left >= 0 && right < len(s) && s[left] == s[right]; left, right = left-1, right+1 {
+	}
+	return left + 1, right - 1
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
