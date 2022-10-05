aecbd85acb60d63418b0d5e208ef96ae66c1eec9
xmh1011 <1197843839@qq.com>
2022-10-05 18:26:29 +0800

    10.5 commit 3.0

---
+++ go/24.go
@@ -0,0 +1,62 @@
+package main
+
+//给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目在范围 [0, 100] 内
+// 0 <= Node.val <= 100
+//
+//
+// Related Topics 递归 链表 👍 1589 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 注意链表交换不是简单的值交换，而是节点交换
+func swapPairs(head *ListNode) *ListNode {
+	cur := &ListNode{0, head}
+	temp := cur
+	for temp.Next != nil && temp.Next.Next != nil {
+		node1 := temp.Next
+		node2 := temp.Next.Next
+		temp.Next = node2
+		node1.Next = node2.Next
+		node2.Next = node1
+		temp = node1
+	}
+	return cur.Next
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
