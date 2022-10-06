ad16552151d72b19cb2683a95183174312af8db8
xmh1011 <1197843839@qq.com>
2022-10-06 21:08:18 +0800

    10.6 commit 3.0 too much TODO

---
+++ go/35.go
@@ -0,0 +1,60 @@
+package main
+
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//
+// 请必须使用时间复杂度为 O(log n) 的算法。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: nums = [1,3,5,6], target = 5
+//输出: 2
+//
+//
+// 示例 2:
+//
+//
+//输入: nums = [1,3,5,6], target = 2
+//输出: 1
+//
+//
+// 示例 3:
+//
+//
+//输入: nums = [1,3,5,6], target = 7
+//输出: 4
+//
+//
+//
+//
+// 提示:
+//
+//
+// 1 <= nums.length <= 10⁴
+// -10⁴ <= nums[i] <= 10⁴
+// nums 为 无重复元素 的 升序 排列数组
+// -10⁴ <= target <= 10⁴
+//
+//
+// Related Topics 数组 二分查找 👍 1741 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func searchInsert(nums []int, target int) int {
+	n := len(nums)
+	left, right := 0, n-1
+	ans := n
+	for left <= right {
+		mid := (right-left)>>1 + left
+		if target <= nums[mid] {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return ans
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
