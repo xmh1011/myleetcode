ef070fd1ddc1e87e1eb99e33721e25d0dafa0762
xmh1011 <1197843839@qq.com>
2023-07-14 15:21:30 +0800

    7.14 commit
    Signed-off-by: xmh1011 <1197843839@qq.com>

---
+++ go/77.go
@@ -0,0 +1,45 @@
+package main
+
+// 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+//
+// 你可以按 任何顺序 返回答案。
+//
+//
+//
+// 示例 1：
+//
+//
+// 输入：n = 4, k = 2
+// 输出：
+// [
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+// ]
+//
+// 示例 2：
+//
+//
+// 输入：n = 1, k = 1
+// 输出：[[1]]
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 20
+// 1 <= k <= n
+//
+//
+// Related Topics 回溯 👍 1424 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func combine(n int, k int) [][]int {
+
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
