ccfd2d8e35ab5ff9267ec12ba2dbddc71e23cb4e
xmh1011 <1197843839@qq.com>
2023-09-26 23:33:33 +0800

    feat: 2023-09-26 commit
    Signed-off-by: xmh1011 <1197843839@qq.com>

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file_path="./README.md"
+
+now=$(date +"%Y-%m-%d %H:%M:%S")
+
+date=$(date +"%Y-%m-%d")
+
+line_number=$(($(wc -l < "$file_path") - 4))
+
+title="- 🗓 Updated at: "
+
+new_content="$title$now"
+
+sed -i '' "${line_number}s/.*/$new_content/" "$file_path"
+
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+
+git pull origin main
+git add .
+git commit -s -m "feat: $date commit"
+git push origin main -f
