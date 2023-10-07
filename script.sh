60f1dcef2ac777dd4857bf6abfceca53b9359323
xmh1011 <1197843839@qq.com>
2023-10-07 19:44:06 +0800

    feat: 2023-10-07 commit
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file_path="./README.md"
+file_path="/Users/xiaominghao/code/myleetcode/README.md"
 
 now=$(date +"%Y-%m-%d %H:%M:%S")
 
@@ -15,6 +15,8 @@ new_content="$title$now"
 sed -i '' "${line_number}s/.*/$new_content/" "$file_path"
 
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+
+cd /Users/xiaominghao/code/myleetcode
 
 git add .
 git commit -s -m "feat: $date commit"
