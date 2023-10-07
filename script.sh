e29587c180b561e2de04de44163bf3cc66e5e4b3
xmh1011 <1197843839@qq.com>
2023-10-07 20:23:24 +0800

    feat: 2023-10-07 commit
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- script.sh
+++ script.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-file_path="/Users/xiaominghao/code/myleetcode"
-
-readme_path="$file_path/README.md"
-
-echo $readme_path
-
-log_path="$file_path/log.txt"
+file_path="./README.md"
 
 now=$(date +"%Y-%m-%d %H:%M:%S")
 
 date=$(date +"%Y-%m-%d")
 
-line_number=$(($(wc -l < "$readme_path") - 4))
+line_number=$(($(wc -l < "$file_path") - 4))
 
 title="- 🗓 Updated at: "
 
@@ -22,10 +16,7 @@ sed -i '' "${line_number}s/.*/$new_content/" "$file_path"
 
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
-cd $file_path
-
-echo "success: $now" >> "$log_path"
-
+git pull origin main
 git add .
 git commit -s -m "feat: $date commit"
 git push origin main -f
