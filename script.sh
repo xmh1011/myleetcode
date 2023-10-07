2d40d4e8cdf262a762f2e58a3000cebbc9b5bd77
xmh1011 <1197843839@qq.com>
2023-10-07 20:13:18 +0800

    feat: 2023-10-07 commit
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ title="- 🗓 Automatically updated by [script](./script.sh) at: "
 
 new_content="$title$now"
 
-sed -i '' "${line_number}/.*/$new_content/" "$readme_path"
+sed -i "" "${line_number}s/.*/$new_content/" "$readme_path"
 
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
