f88e86b664e1856f99d9895515c0ed4af21e3890
xmh1011 <1197843839@qq.com>
2023-10-07 20:17:47 +0800

    feat: 2023-10-07 commit
    Signed-off-by: xmh1011 <1197843839@qq.com>

--- script.sh
+++ script.sh
@@ -18,6 +18,8 @@ new_content="$title$now"
 
 sed -i '' "${line_number}s/.*/$new_content/" "$readme_path"
 
+echo "sed -i '' "${line_number}s/.*/$new_content/" "$readme_path""
+
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
 
 cd $file_path
